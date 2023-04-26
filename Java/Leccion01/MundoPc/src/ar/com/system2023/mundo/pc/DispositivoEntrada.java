6a31f76757121ea26fcbc24579809f5d1c26cad7
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2023-04-26 20:58:22 -0300

    python config

--- Java/Leccion01/MundoPc/src/ar/com/system2023/mundo/pc/DispositivoEntrada.java
+++ Java/Leccion01/MundoPc/src/ar/com/system2023/mundo/pc/DispositivoEntrada.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ar.com.system2023.mundo.pc;
 
-/**
- *
- * @author gonxa
- */
 public class DispositivoEntrada {
     
 }
