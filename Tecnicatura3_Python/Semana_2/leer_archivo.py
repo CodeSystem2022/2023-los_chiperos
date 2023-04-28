4cd262d3faa3e700a137ef35a30cb16778fe4024
Angelo Martin Pertus <angelopertus16@gmail.com>
2023-04-28 20:34:08 -0300

    manejo de exep y leer archivos

---
+++ Tecnicatura3_Python/Semana_2/leer_archivo.py
@@ -0,0 +1,7 @@
+archivo = open(' C:\Users\angel\OneDrive\Escritorio\2023-los_chiperos\Tecnicatura3_Python\Semana_2 prueba.txt , "a", encoding="utf8') # las letras son:"r= read""a= append""x=" "w= write"
+#print(archivo.read())
+#print(archivo.read(16))
+#print(archivo.read(10))
+print(archivo.readline())
+print(archivo.readline())
+
