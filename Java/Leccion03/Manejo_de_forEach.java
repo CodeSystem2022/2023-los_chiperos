c8b96f4661a987cf6e6323b24002c68f1027929a
Angelo Martin Pertus <angelopertus16@gmail.com>
2023-05-02 20:26:50 -0300

    1.1 java

---
+++ Java/Leccion03/Manejo_de_forEach.java
@@ -0,0 +1,10 @@
+package test;
+
+public class TestForEach {
+    public static void main(String[] args) {
+        int edades[] = {5, 6, 7, 8, 9}; // sintaxis resumida
+        for (int edad: edades) { //sintaxis de forEach
+            System.out.println("edad = " + edad);
+        }
+    }
+}
