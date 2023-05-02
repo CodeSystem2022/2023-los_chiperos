f9f52941a0f2df8078216710c36001f2e412ecec
Juan Schallmoser <schallmoserjuan@gmail.com>
2023-05-02 19:43:31 -0300

    Semana 1 de Java concluida

--- Java/Leccion01/MundoPc/src/ar/com/system2023/mundo/pc/DispositivoEntrada.java
+++ Java/Leccion01/MundoPc/src/ar/com/system2023/mundo/pc/DispositivoEntrada.java
@@ -1,5 +1,7 @@
 package ar.com.system2023.mundo.pc;
 
 public class DispositivoEntrada {
-    
+
+    public DispositivoEntrada(String tipoEntrada, String marca) {
+    }
 }
--- Java/Leccion01/MundoPc/src/ar/com/system2023/mundo/pc/teclado.java
+++ Java/Leccion01/MundoPc/src/ar/com/system2023/mundo/pc/teclado.java
@@ -1,18 +1,18 @@
-package ar.com.system.mundopc;
+package ar.com.system2023.mundo.pc;
 
 public class Teclado extends DispositivoEntrada {
   private final int idTeclado;
   private static int contadorteclados;
-  public static int contadorteclados;
   
   public Teclado(String TipoEntrada, String marca){
     super(tipoEntrada, marca);
     this.idTeclado = ++Teclado.contadorTeclados;
   }
+
   @Override
-  public String toString(){
-    
-    return "Teclado{" + "idTeclado=" + idTeclado +", "+super.toString()+'};
-    
+  public String toString() {
+    return "Teclado{" +
+            "idTeclado=" + idTeclado + ", "+super.toString()+
+            '}';
   }
 }
