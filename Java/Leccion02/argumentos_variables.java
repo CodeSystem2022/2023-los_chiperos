bf42ace121300978e1feaa9ee590076fc56e7746
Fernando Nicolas Zelarayan <98678489+NicolasZelarayan@users.noreply.github.com>
2023-05-01 21:58:46 -0300

    Update argumentos_variables.java

---
+++ Java/Leccion02/argumentos_variables.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestArgumentoVariables {
+    public state void main (string[] args) {
+    
+    }
+    
+    private static void imprimirNumeros(int ...numeros){
+        for (int i = 0; i < numeros.lenght; i++) {
+            System.out.println("Elementos: "+numeros[i];
+            }
+     }
+}
