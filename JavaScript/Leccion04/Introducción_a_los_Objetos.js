0881e76155fe4a8922a1f7e13878a08e7aec6425
Angelo Martin Pertus <angelopertus16@gmail.com>
2023-05-04 20:22:32 -0300

    introducción a los Objetos

---
+++ JavaScript/Leccion04/Introducción_a_los_Objetos.js
@@ -0,0 +1,17 @@
+// introducción a los Objetos
+let x = 10
+console.log(x.length);
+
+//objeto
+let persona = {
+    nombre: 'Carlos', 
+    apellido: 'Gil',
+    email: 'cgil@gmail.com',
+    edad: 30
+}
+
+console.log(persona.nombre); Carlos
+console.log(persona.apellido); Gil
+console.log(persona.email); 'egil@gmail.com'
+console.log(persona.edad); 30
+
