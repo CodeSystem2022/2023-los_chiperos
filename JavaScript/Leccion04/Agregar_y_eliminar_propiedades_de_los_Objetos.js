7c3b3def44f284472f6bdf83a4d511a6b716c562
Angelo Martin Pertus <angelopertus16@gmail.com>
2023-05-04 21:04:21 -0300

    1.5 java script

---
+++ JavaScript/Leccion04/Agregar_y_eliminar_propiedades_de_los_Objetos.js
@@ -0,0 +1,35 @@
+let x = 10
+console.log(x.length);
+
+//objeto
+let persona = {
+    nombre: 'Carlos', 
+    apellido: 'Gil',
+    email: 'cgil@gmail.com',
+    edad: 30,
+    nombreCompleto: function(){
+        return this.nombre+' '+this.apellido;
+    }
+}
+
+console.log(persona.nombre); Carlos
+console.log(persona.apellido); Gil
+console.log(persona.email); 'egil@gmail.com'
+console.log(persona.edad); 30
+console.log(nombreCompleto()); Carlos_Gil
+
+let persona2 = new Object();
+persona2.nombre = 'Juan';
+persona2.direccion = 'Larrea 405';
+persona2.telefono = '3755768798';
+console.log(persona2.direccion) 
+
+//for in
+for(propiedad in persona){
+    console.log(propiedad); nombre, apellido, email, edad, nombreCompleto
+    console.log(persona[propiedad]); Carlos, Gil, 30
+}
+
+persona.apellida = 'Pertus';
+delete persona.apellida;
+console.LOG(persona); 
