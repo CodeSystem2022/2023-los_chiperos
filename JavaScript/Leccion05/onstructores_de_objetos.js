7567340df4d691ebc103d318d9c063693f1bbe94
Angelo Martin Pertus <angelopertus16@gmail.com>
2023-05-09 20:43:56 -0300

    5.3 java script

--- JavaScript/Leccion05/onstructores_de_objetos.js
+++ JavaScript/Leccion05/onstructores_de_objetos.js
@@ -1,13 +1,17 @@
 function persona3(nombre, apellido, email){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.email = email
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre+' '+this.apellido;
+    }
 }
 
 let padre = new persona3('leo', 'lopez', 'lopez@gmail.com'); 
 padre.nombre = 'luis';
-console.log(padre)
+console.log(padre);
+console.log(padre.nombreCompleto());
 
-let madre = new persona3('laura', 'contrera', 'contreral@gmail.com')
-console.log(madre)
-
+let madre = new persona3('laura', 'contrera', 'contreral@gmail.com');
+console.log(madre);
+console.log(madre.nombreCompleto());
