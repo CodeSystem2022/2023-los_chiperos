6671fbe5053ac840bc85fec55db9749853dc5b25
Juan Schallmoser <schallmoserjuan@gmail.com>
2023-05-09 21:05:57 -0300

    5.9 Eliminar varios registros

---
+++ Python/Semana_4/BD/delete_varios_reg.py
@@ -0,0 +1,26 @@
+import psycopg2 # Esto es para poder conectarnos a Postgressql
+
+#Conectarnos a la base de datos
+conexion = psycopg2.connect(
+    user = 'postgres',
+    password = 'admin',
+    host = '127.0.0.1',
+    port = '5432',
+    database = 'test_bd'
+)
+
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM persona WHERE id_persona IN %s'
+            entrada = input('Digite los números de registros a eliminar (separados por coma): ')
+            valores = (tuple(entrada.split(',')),) # Tupla de valores
+            cursor.execute(sentencia, valores) # De esta manera ejecutamos la sentencia
+            registros_eliminados = cursor.rowcount
+            print(f'Los registros eliminados son: {registros_eliminados}')
+            
+
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
